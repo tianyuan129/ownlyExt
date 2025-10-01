3bf8b3b5d13eb98f615f4c1a266ebda14564dd40
tankmaster48 <craigshuaogu@gmail.com>
2025-10-01 11:46:30 -0700

    add full paper retrieval tool

--- ragless/llm.py
+++ ragless/llm.py
@@ -1,26 +1,84 @@
 from fastmcp import FastMCP
 import sys
 
-from ollama import chat
-from ollama import ChatResponse
+from ollama import ChatResponse, chat
 
-mcp = FastMCP("NDN RAG")
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader
+
+def fetch_paper(index: int) -> str:
+  """
+  Fetch an entire paper
+
+  0. HTTP as the narrow waist of the future internet
+
+  Args:
+    index (int): The index of the paper from the list to search
+
+  Returns:
+    str: The retrieved paper
+  """
+
+  file_path = "/srv/llama/docs/random/httpwaist.pdf"
+
+  loader = PyPDFLoader(file_path)
+
+  text = ""
+  for page in loader.lazy_load():
+    text = text + page.page_content
+
+  document = Document(
+    page_content=text
+  )
+
+  return text
+
+
+
+mcp = FastMCP("Paper RAG")
 
 @mcp.tool
 def ndn_qa(message: str) -> str:
-    messages = [
-        {"role" : "system", "content" : """You are an AI agent that provides reasoning for tasks regarding Named-Data Networking (NDN).
-         - Do not mix TCP/IP reasoning or logic with reasoning about NDN. Only use information you know about NDN. The other agent can answer questions about NDN.
-         - If you are unsure about an answer or you do not know the answer to a question, say so in your response.
-         - If answering, provide a detailed response, ensuring you completely answer the user's question.
-         """}
-    ]
-    messages.append(                                                                                
-        {"role" : "user", "content" : message}                                                            
-    )                                                                                               
-                                                                                                    
-    response: ChatResponse = chat(model='gpt-oss:20b', messages=messages)
+    user_message = {'role': 'user', 'content': 'What is S-GET as defined in the HTTP at the narrow waist paper?'}
 
-    return response.message.content
+    messages = [
+    {'role': 'system', 'content': '''You are an AI assistant that answers questions about networking.
+        - Use tools to respond when necessary
+        - Make sure to fully answer the user's question
+        - Answers should have enough detail and context but still be concise'''},
+        user_message
+    ]
+    print('Prompt:', messages[1]['content'])
+
+    available_functions = {
+        'fetch_paper': fetch_paper,
+    }
+
+    response: ChatResponse = chat(
+        'gpt-oss:20b',
+        messages=messages,
+        tools=[fetch_paper],
+    )
+
+    if response.message.tool_calls:
+        # There may be multiple tool calls in the response
+        for tool in response.message.tool_calls:
+            # Ensure the function is available, and then call it
+            if function_to_call := available_functions.get(tool.function.name):
+                output = function_to_call(**tool.function.arguments)
+    else:
+       return response.message
+
+    # Only needed to chat with the model using the tool call results
+    if response.message.tool_calls:
+        # Add the function response to messages for the model to use
+        messages.append(response.message)
+        messages.append({'role': 'tool', 'content': str(output), 'tool_name': tool.function.name})
+        messages.append(user_message)
+
+        # Get final response from model with function outputs
+        final_response = chat('gpt-oss:20b', messages=messages)
+
+    return final_response.content
 
 mcp.run()
